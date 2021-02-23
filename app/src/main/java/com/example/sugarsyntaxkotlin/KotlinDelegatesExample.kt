2c3705f76de72dc8b2d980728656ab99aa4dd2ec
Roberto Osorio <robertoosorio.s@gmail.com>
2021-02-23 16:19:20 -0300

    add delegate for var

--- app/src/main/java/com/example/sugarsyntaxkotlin/KotlinDelegatesExample.kt
+++ app/src/main/java/com/example/sugarsyntaxkotlin/KotlinDelegatesExample.kt
@@ -1,12 +1,23 @@
 package com.example.sugarsyntaxkotlin
 
-import kotlin.reflect.*
+import kotlin.reflect.KMutableProperty0
+import kotlin.reflect.KMutableProperty1
+import kotlin.reflect.KProperty
+import kotlin.reflect.KProperty0
 
-class ReferencedProperty<T>(private val get: () -> T) {
+class ReferencedProperty<T>(private val get: () -> T,
+                            private val set: (T) -> Unit = {}) {
 
     operator fun getValue(thisRef: Any?,
                           property: KProperty<*>): T = get()
+
+    operator fun setValue(thisRef: Any?,
+                          property: KProperty<*>,
+                          value: T) = set(value)
 }
+
+fun <T> ref(property: KMutableProperty0<T>) = ReferencedProperty(property::get,
+    property::set)
 
 fun <T> ref(property: KProperty0<T>) = ReferencedProperty(property::get)
 
